cc56820231476cc1be899bd8a630e7efe228277e
Ross Simpson <ross.simpson@konga.com>
2016-03-17 15:13:15 +0200

    Supressed "Synk" wizard messages, with an option to unsurpress them

--- README.md
+++ README.md
@@ -5,4 +5,14 @@ Builds and image with "snyk" and "nps", and a "/tmp/scan.sh" script to look for 
 
 To start this container:
 
-    docker run -v {node_project_path}:/src -e NPM_REGISTRY="{custom_npm_registry}" node_security_scan
+    docker run -v {node_project_path}:/src node_security_scan
+
+Additionally environment variables can be passed in
+
+1. Specify an NPM registry to use (eg: if you're running a private npm registry)
+
+    -e NPM_REGISTRY="{custom_npm_registry}"
+
+2. Prevent the suppression of snyk messages (mostly around running the wizard)
+
+    -e SNYK_NOISY=true
--- scan.sh
+++ scan.sh
@@ -19,9 +19,18 @@ if [ $? -eq 0 ]; then
 
     echo 'Synk security check:'
     echo '--------------------'
-    snyk test
 
-    echo
+    # by default will supress some output, this is personal preference but can be overridden with a SNYK_NOISY env var
+    if [ "$SNYK_NOISY" ]; then
+        snyk test
+
+    else
+        snyk test \
+            | grep -v 'No direct dependency upgrade can address this issue.' \
+            | grep -v 'Run `snyk wizard` to explore remediation options.' \
+            | grep -v 'Run `snyk wizard` to address these issues.'
+    fi
+
     echo 'NSP security check:'
     echo '--------------------'
     nsp check
