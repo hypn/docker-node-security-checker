bb348dc09f2d5c68be7f27800bca650ac194ea94
Ross Simpson <ross.simpson@konga.com>
2016-03-23 12:21:17 +0200

    Fixed line endings

--- scan.sh
+++ scan.sh
@@ -15,9 +15,9 @@ else
     snyk test \
         | grep -v 'No direct dependency upgrade can address this issue.' \
         | grep -v 'Run `snyk wizard` to explore remediation options.' \
-        | grep -v 'Run `snyk wizard` to address these issues.'
-        | grep -v 'Next steps:'
-        | grep -v '- Run `snyk monitor` to be notified about new related vulnerabilities.'
+        | grep -v 'Run `snyk wizard` to address these issues.' \
+        | grep -v 'Next steps:' \
+        | grep -v '- Run `snyk monitor` to be notified about new related vulnerabilities.' \
         | grep -v '- Run `snyk test` as part of your CI/test.'
 fi
 
